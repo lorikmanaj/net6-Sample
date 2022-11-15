0022a991cfc3f2aa37b82ce8244e7154469fd7d2
Lik <lorikmanaj@hotmail.com>
2022-11-16 00:36:37 +0100

    Added claims and jwt login

--- HotelListing/HotelListing.API/Services/AuthManager.cs
+++ HotelListing/HotelListing.API/Services/AuthManager.cs
@@ -3,6 +3,12 @@ using HotelListing.API.Contracts;
 using HotelListing.API.Data;
 using HotelListing.API.Models.Users;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using JwtRegisteredClaimNames = System.IdentityModel.Tokens.Jwt.JwtRegisteredClaimNames;
 
 namespace HotelListing.API.Services
 {
@@ -10,37 +16,30 @@ namespace HotelListing.API.Services
     {
         private readonly IMapper _mapper;
         private readonly UserManager<ApiUser> _userManager;
-        
-        public AuthManager(IMapper mapper, UserManager<ApiUser> userManager)
+        private readonly IConfiguration _configuration;
+
+        public AuthManager(IMapper mapper, UserManager<ApiUser> userManager, IConfiguration configuration)
         {
             this._mapper = mapper;
             this._userManager = userManager;
+            this._configuration = configuration;
         }
 
-        public async Task<bool> Login(LoginDto loginDto)
+        public async Task<AuthResponseDto> Login(LoginDto loginDto)
         {
-            //bool isValidUser;
-            //try
-            //{
-            //    var user = await _userManager.FindByEmailAsync(loginDto.Email);
-            //    isValidUser = await _userManager.CheckPasswordAsync(user, loginDto.Password);
-            //}
-            //catch (Exception)
-            //{
+            var user = await _userManager.FindByEmailAsync(loginDto.Email);
+            bool isValidUser = await _userManager.CheckPasswordAsync(user, loginDto.Password);
 
-            //    throw;
-            //}
-            //return isValidUser;
-            var _user = await _userManager.FindByEmailAsync(loginDto.Email);
+            if (user == null || isValidUser == false)
+                return null;
 
-            if (_user is null)
-                return false;
+            var token = await GenerateToken(user);
 
-            //bool isValidCredentials =
-            return await _userManager.CheckPasswordAsync(_user, loginDto.Password);
-
-            //if (!isValidCredentials)
-            //    return true;
+            return new AuthResponseDto
+            {
+                Token = token,
+                UserId = user.Id
+            };
         }
 
         public async Task<IEnumerable<IdentityError>> Register(ApiUserDto userDto)
@@ -54,6 +53,37 @@ namespace HotelListing.API.Services
                 await _userManager.AddToRoleAsync(user, "User");
 
             return result.Errors;
+        }
+
+        private async Task<string> GenerateToken(ApiUser user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JwtSettings:Key"]));
+
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var roles = await _userManager.GetRolesAsync(user);
+
+            var roleClaims = roles.Select(x => new Claim(ClaimTypes.Role, x)).ToList();
+
+            var userClaims = await _userManager.GetClaimsAsync(user);
+
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.Email),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                new Claim("uid", user.Id)
+            }.Union(userClaims).Union(roleClaims);
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["JwtSettings:Issuer"],
+                audience: _configuration["JwtSettings:Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(Convert.ToInt32(_configuration["JwtSettings:DurationInMinutes"])),
+                signingCredentials: credentials
+                );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
         }
     }
 }
