399421ed1cebb212922319997fb959d0568cbdb6
Lik <lorikmanaj@hotmail.com>
2023-02-23 15:47:54 +0100

    saving state - lindi

--- HotelListing/HotelListing.API/Controllers/AccountController.cs
+++ HotelListing/HotelListing.API/Controllers/AccountController.cs
@@ -11,10 +11,12 @@ namespace HotelListing.API.Controllers
     public class AccountController : ControllerBase
     {
         private readonly IAuthManager _authManager;
+        private readonly ILogger<AccountController> _logger;
 
-        public AccountController(IAuthManager authManager)
+        public AccountController(IAuthManager authManager, ILogger<AccountController> logger)
         {
             this._authManager = authManager;
+            this._logger = logger;
         }
 
         // POST: api/Account/Register
@@ -25,6 +27,8 @@ namespace HotelListing.API.Controllers
         [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<ActionResult> Register([FromBody] ApiUserDto apiUserDto)
         {
+            _logger.LogInformation($"Registration Attempt for {apiUserDto.Email}");
+
             var errors = await _authManager.Register(apiUserDto);
 
             if (errors.Any())
@@ -68,6 +72,8 @@ namespace HotelListing.API.Controllers
         [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<ActionResult> Login([FromBody] LoginDto loginDto)
         {
+            _logger.LogInformation($"Login Attempt for {loginDto.Email}");
+
             var authResponse = await _authManager.Login(loginDto);
 
             if (authResponse == null)
--- HotelListing/HotelListing.API/Controllers/CountriesController.cs
+++ HotelListing/HotelListing.API/Controllers/CountriesController.cs
@@ -5,6 +5,7 @@ using HotelListing.API.Models.Country;
 using AutoMapper;
 using HotelListing.API.Contracts;
 using Microsoft.AspNetCore.Authorization;
+using HotelListing.API.Exceptions;
 
 namespace HotelListing.API.Controllers
 {
@@ -15,11 +16,13 @@ namespace HotelListing.API.Controllers
     {
         private readonly IMapper _mapper;
         private readonly ICountriesRepository _countriesRepository;
+        private readonly ILogger<CountriesController> _logger;
 
-        public CountriesController(IMapper mapper, ICountriesRepository countriesRepository)
+        public CountriesController(IMapper mapper, ICountriesRepository countriesRepository, ILogger<CountriesController> logger)
         {
             this._mapper = mapper;
             this._countriesRepository = countriesRepository;
+            this._logger = logger;
         }
 
         // GET: api/Countries
@@ -41,7 +44,7 @@ namespace HotelListing.API.Controllers
             var country = await _countriesRepository.GetDetails(id);
 
             if (country == null)
-                return NotFound();
+                throw new NotFoundException(nameof(GetCountry), id);
 
             var countryDto = _mapper.Map<GetCountryDetailsDto>(country);
 
